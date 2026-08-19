d7676ca4c8d6b543caeb5c1f829db5556070b3d5
Mr-Code-start <vitalybaca92@gmail.com>
2026-08-19 08:03:56 -0500

    Initial commit

---
+++ src/main/java/com/sanuvi/ferova/apirest/ApiRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sanuvi.ferova.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanuvi/ferova/apirest/ApiRestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanuvi.ferova.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
