ba35d77cb82450ded8b86a0c378372d80e8d20e2
Mr-Code-start <vitalybaca92@gmail.com>
2026-08-19 12:02:41 -0500

    feat(shared): agregar clase base de raíz aggregate auditable
    - Esta clase proporciona capacidades de auditoría y publicación de eventos para todos los agregados del dominio

---
+++ src/main/java/com/sanuvi/ferova/apirest/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,35 @@
+package com.sanuvi.ferova.apirest.shared.domain.model.aggregates;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+
+import java.time.LocalDate;
+
+/**
+ * Es una clase base para agregados que requieren auditoría.
+ * Hereda de AbstractAggregateRoot (de Spring)
+ * y usa anotaciones de auditoría propias de MongoDB
+ * (aunque usa @CreatedDate y @LastModifiedDate de Spring Data común).
+ * El tipo genérico T representa el propio aggregate root concreto.
+ * @param <T>
+ */
+@Getter
+public class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+    @Id
+    private Long id;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updateAt;
+
+    public void addDomainEvent(Object event) {
+        registerEvent(event);
+    }
+}
