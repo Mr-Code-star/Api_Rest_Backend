275fe40029dfdb09efe9a9452a1d85be5f5acc48
Mr-Code-start <vitalybaca92@gmail.com>
2026-08-20 13:55:27 -0500

    feat: add enum Roles

---
+++ src/main/java/com/sanuvi/ferova/apirest/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,13 @@
+package com.sanuvi.ferova.apirest.iam.domain.model.valueobjects;
+
+/**
+ * Enum que define los nombres de los roles disponibles en el sistema
+ * <p>
+ *     Este enum es la fuente de verdad para los nombres de los roles.
+ *     para almacenar los roles en la base de datos.
+ **/
+public enum Roles {
+    MOTHER,
+    NURSE,
+    ADMIN
+}
